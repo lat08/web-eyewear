78e8b40b0676178a1edcfc543207f50ea9cccb86
TuanPTT <phanthanhthaituan452005@gmail.com>
2026-02-25 22:27:08 +0700

    fix: img

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-        port: "",
-        pathname: "**",
-      },
-    ],
+    domains: ["kinhmatlily.com"],
+    unoptimized: true,
   },
 };
 
